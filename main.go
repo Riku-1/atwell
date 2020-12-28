5cbd9e040920b1545dbc6a9060641929f30cf1ce
Riku-1 <osaka.riku@gmail.com>
2020-12-28 18:26:16 +0900

    refactor:Deleted sample code

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"golang-api/repository"
 	"golang-api/usecase"
 	"golang-api/web/handler"
@@ -9,13 +8,7 @@ import (
 	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to the HomePage")
-}
-
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-
 	au := usecase.NewArticleUsecase(repository.NewDummyArticleRepository())
 	ah := handler.ArticleHandler{Usecase: au}
 	handler.HandleArticleRequest(ah)
