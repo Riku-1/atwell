5f40ff1573577959ed28a25f06de470e94968ee1
Riku-1 <osaka.riku@gmail.com>
2020-12-31 13:18:09 +0900

    feature(#16):Add column to articles table

--- migration/20201230122050_create_artcles_table.sql
+++ migration/20201230122050_create_artcles_table.sql
@@ -4,7 +4,10 @@ CREATE TABLE articles (
     id int NOT NULL primary key,
     title varchar(255) NOT NULL,
     body text NOT NULL,
-    publish_date datetime
+    publish_date datetime,
+    created_at datetime,
+    updated_at datetime,
+    deleted_at datetime
 );
 -- +goose StatementEnd
 
